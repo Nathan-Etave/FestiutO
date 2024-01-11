0d9e967378cefde432fb35a0d90fd1308bc90934
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-01-11 16:58:28 +0100

    Background des programmes

--- festiuto/static/js/programme.js
+++ festiuto/static/js/programme.js
@@ -3,7 +3,9 @@ function toggle_concert(id) {
     var element = document.getElementById(id);
     if (element.style.display == "none") {
         element.style.display = "block";
+        element.style.backgroundColor = "";
     } else {
         element.style.display = "none";
+        element.style.backgroundColor = "#4b50b4";
     }
 }
